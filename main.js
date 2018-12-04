a4de192a1d202eba2c3870e2b597be3d4df75633
Michele Papagni <michele_papagni@me.com>
2018-12-04 13:53:39 +0100

    added clone

--- main.js
+++ main.js
@@ -1,20 +1,6 @@
-var clearDiv = '<div class="cl-t"></div>',
-messageSentTagOpen = '<div class="message sent">',
-messageReceivedTagOpen = '<div class="message received">',
-messageTagClose = '</div>',
-spanTimeOpen = '<span class="message-time">',
-spanTimeClose = '</span>',
-messageOptionsButton = '<i class="fa fa-chevron-down f-right message-options"></i>',
-optionsPanel = `<div class="message-options-panel">
-	<div class="message-info">Info messaggio</div>
-	<div class="message-destroy">Cancella messaggio</div>
-</div>`,
-contactsFilter = $('#contacts-filter'),
-contactsContainer = $('.contacts'),
-messagesContainer = $('.right-messages-container');
+var messagesContainer = $('.right-messages-container .right-messages');
 
 $(document).ready(function() {
-
 	$('.new-message-inputs').keypress(function(e) {
 		if (e.which == 13) {
 			var thisInput = $(this),
@@ -23,17 +9,17 @@ $(document).ready(function() {
 
 			if (newMessage) {
 				//now i can append to the messages
-				var footerCnt = thisInput.parent(),
-				rightContainer = footerCnt.parent()
-				messagesContainer = rightContainer.children('.right-messages-container').children('.right-messages.active');
+				var messageTemplate = $('.template .message.sent').clone();
+				messageTemplate.children('.message-text').text(newMessage);
+				messageTemplate.children('.message-time').text(now.getHours() + ':' + now.getMinutes());
 
-				messagesContainer.append(
-					messageSentTagOpen + newMessage + messageOptionsButton + spanTimeOpen + now.getHours() + ':' + now.getMinutes() + spanTimeClose + optionsPanel + messageTagClose + clearDiv
-				);
+				messagesContainer.append(messageTemplate);
+
+				var clearTemplate = $('.template .cl-t').clone();
+				messagesContainer.append(clearTemplate);
 
 				thisInput.val('');
 			}
 		}
 	});
-
 });
